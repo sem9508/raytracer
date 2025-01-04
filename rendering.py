1fa5a6d852f09374d43a19e26869c0b9be4641e9
sem9508 <sem40911@gmail.com>
2025-01-04 12:04:37 +0100

    made a molecule:)

--- rendering.py
+++ rendering.py
@@ -14,25 +14,28 @@ def render(width, height):
     samples = 4
     depth = 20
     camera_origin = Vec3(0, 0, 0)
-
     spheres = [
-        Sphere(Vec3(0, -1.5, -10), 2.5, Vec3(100, 100, 255), 80),
-        Sphere(Vec3(-4, -1, -12), 1.5, Vec3(255, 0, 0), 20),
-        Sphere(Vec3(3, 0, -8), 1, Vec3(0, 255, 0), 30),
-        Sphere(Vec3(-1.5, 1, -6), 0.75, Vec3(255, 255, 0), 50),
-        Sphere(Vec3(1.5, 1.5, -9), 1.25, Vec3(255, 165, 0), 40),
-        Sphere(Vec3(-3, -0.5, -7), 1, Vec3(75, 0, 130), 10),
-        Sphere(Vec3(0, -0.3, -4), 0.4, Vec3(0, 255, 255), 5),
-        Sphere(Vec3(2.5, -1.2, -9), 0.5, Vec3(255, 20, 147), 60),
+        Sphere(Vec3(0, 0, -5), 1, Vec3(0, 0, 0), 100),
+
+        Sphere(Vec3(0, 1.3, -5), 0.5, Vec3(255, 255, 255), 10),
+        Sphere(Vec3(1, -0.7, -4.35), 0.5, Vec3(255, 255, 255), 10),
+        Sphere(Vec3(-1, -0.7, -4.35), 0.5, Vec3(255, 255, 255), 10),
+        Sphere(Vec3(0, -0.7, -5.65), 0.5, Vec3(255, 255, 255), 10),
     ]
+
+
+
 
     lights = [
         Light(Vec3(0, 10, -5), 1.2, Vec3(255, 250, 240)),
-        Light(Vec3(-10, 5, -10), 0.8, Vec3(255, 200, 150)),
-        Light(Vec3(10, 10, -15), 1, Vec3(150, 200, 255)),
+        Light(Vec3(-5, 5, -10), 0.8, Vec3(255, 200, 150)),
+        Light(Vec3(5, 5, -10), 1, Vec3(150, 200, 255)),
     ]
 
-    
+
+
+
+        
     image = [[Vec3() for _ in range(width)] for _ in range(height)]
     last_percentage = 0
 
